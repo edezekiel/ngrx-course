4b18b4e1897aa1b8f6efe1781ba0402a2ae08d09
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-03-09 14:36:37 -0600

    Add create and delete operations for course entity

--- src/app/courses/courses-card-list/courses-card-list.component.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.ts
@@ -1,13 +1,8 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output
-} from "@angular/core";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { EditCourseDialogComponent } from "../edit-course-dialog/edit-course-dialog.component";
 import { Course } from "../model/course";
+import { CourseEntityService } from "../services/course-entity.service";
 import { defaultDialogConfig } from "../shared/default-dialog-config";
 
 @Component({
@@ -22,7 +17,10 @@ export class CoursesCardListComponent implements OnInit {
   @Output()
   courseChanged = new EventEmitter();
 
-  constructor(private dialog: MatDialog) {}
+  constructor(
+    private dialog: MatDialog,
+    private coursesService: CourseEntityService
+  ) {}
 
   ngOnInit() {}
 
@@ -41,5 +39,7 @@ export class CoursesCardListComponent implements OnInit {
       .subscribe(() => this.courseChanged.emit());
   }
 
-  onDeleteCourse(course: Course) {}
+  onDeleteCourse(course: Course) {
+    this.coursesService.delete(course);
+  }
 }
--- src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/courses/edit-course-dialog/edit-course-dialog.component.ts
@@ -59,6 +59,8 @@ export class EditCourseDialogComponent {
     if (this.mode === "update") {
       this.coursesService.update(course);
       this.dialogRef.close();
+    } else if (this.mode === "create") {
+      this.coursesService.add(course);
     }
   }
 }
